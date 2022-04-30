bae89248cf7567f9a65a7f0cf9eaf12d488440bc
LeonardoTeixera23 <Leo.dst23@gmail.com>
2022-04-30 15:38:53 -0300

    Adicionando laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@700&display=swap" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Escola blabla</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<header>
+    <div class="conteiner-nav">
+        <h1><img src="./src/logo-escola.png" alt="Logo da escola"></h1>
+        <nav>
+            <ul class="menu" >
+                <li><a href="">Home</a></li>
+                <li><a href="">Unidades</a></li>
+                <li><a href="">Entrar/Cadastrar</a>
+                    <ul class="menu-ocuto">
+                        <li> <a href="#">Entrar</a></li>
+                        <li> <a href="#">Cadatrar</a></li>
+                    </ul>
+                </li>
+
+            </ul>
+        </nav>
+    </div>
+</header>
+<body>
+    
+</body>
+</html>
